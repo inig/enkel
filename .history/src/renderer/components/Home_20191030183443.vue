23079caa0e0482083c2333fd7598fceacbd452d6
liangshan <loveliangshan@126.com>
2019-10-30 18:40:05 +0800

    no message

---
+++ .history/src/renderer/components/Home_20191030183443.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Button type="success">测试</Button>
+  </div>
+</template>
+
+<script>
+  import { Button } from 'view-design'
+  export default {
+    name: 'Home',
+    components: {
+      Button
+    }
+  }
+</script>
+
+<style scoped>
+</style>
