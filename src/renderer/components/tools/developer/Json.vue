391f84e8de77041e4f769270bce84c14d1faef49
梁山 <liangshan@liangshandeMacBook-Pro.local>
2019-12-25 01:18:38 +0800

    文本查找

--- src/renderer/components/tools/developer/Json.vue
+++ src/renderer/components/tools/developer/Json.vue
@@ -36,106 +36,161 @@
 </template>
 
 <script>
-  import { Input } from 'view-design'
-  import Copy from '../../custom/Copy'
-  // import '../../../../../static/js/jquery.json'
-  // import '../../../../../static/js/json2'
-  // import '../../../../../static/js/jsonlint'
-  export default {
-    name: 'Json',
-    components: {
-      Input,
-      Copy,
-      JsonView: () => import('../../custom/json-view'),
-      ESplit: () => import('../../custom/ESplit')
-    },
-    data () {
-      return {
-        split: 0.5,
-        originValue: '',
-        // currentJson: '',
-        // currentJsonStr: ''
+import { Input } from 'view-design'
+import Copy from '../../custom/Copy'
+import { remote } from 'electron'
+import { FindInPage } from 'electron-find'
+// import '../../../../../static/js/jquery.json'
+// import '../../../../../static/js/json2'
+// import '../../../../../static/js/jsonlint'
+export default {
+  name: 'Json',
+  components: {
+    Input,
+    Copy,
+    JsonView: () => import('../../custom/json-view'),
+    ESplit: () => import('../../custom/ESplit')
+  },
+  data () {
+    return {
+      split: 0.5,
+      originValue: '',
+      findInPage: null,
+      inputEvent: null
+      // currentJson: '',
+      // currentJsonStr: ''
+    }
+  },
+  computed: {
+    targetValue () {
+      let out = {}
+      try {
+        out = JSON.parse(this.originValue)
+      } catch (err) {
+        out = {}
       }
-    },
-    computed: {
-      targetValue () {
-        let out = {}
-        try {
-          out = JSON.parse(this.originValue)
-        } catch (err) {
-          out = {}
+      return out
+    }
+    // transformedValue () {
+    //   let result = ''
+    //   if (this.originValue !== '') {
+    //     let originValue = this.originValue
+    //     try {
+    //       this.currentJson = jsonlint.parse(originValue)
+    //       this.currentJsonStr = JSON.stringify(this.currentJson)
+    //       result = new JSONFormat(originValue, 4).toString()
+    //     } catch (e) {
+    //       result = '<span style="color: #f1592a; font-weight: bold;">' + e + '</span>'
+    //       this.currentJsonStr = result
+    //     }
+    //     return result
+    //   } else {
+    //     return ''
+    //   }
+    // }
+  },
+  mounted () {
+    this.findInPage = new FindInPage(remote.getCurrentWebContents(), {
+      parentElement: document.querySelector('.transformed_box'),
+      boxBgColor: '#333',
+      boxShadowColor: '#000',
+      inputColor: '#aaa',
+      inputBgColor: '#222',
+      inputFocusColor: '#555',
+      textColor: '#aaa',
+      textHoverBgColor: '#555',
+      caseSelectedColor: '#555',
+      offsetTop: 63,
+      offsetRight: 18,
+      preload: true
+    })
+
+    this.inputEvent = document.createEvent('HTMLEvents')
+    this.inputEvent.initEvent('input', true, true)
+
+    this.$nextTick(() => {
+      let isMac = (navigator.platform === 'Mac68K') || (navigator.platform === 'MacPPC') || (navigator.platform === 'Macintosh') || (navigator.platform === 'MacIntel')
+      isMac && (this.platform = 'mac')
+      window.onkeydown = (ev) => {
+        if (this.$route.name === 'json') {
+          if (this.platform === 'mac') {
+            if (ev.metaKey && ev.keyCode === 70) {
+              // mac下 command + shift + s
+              this.toggleFilterInput()
+            }
+          } else {
+            if (ev.ctrlKey && ev.keyCode === 70) {
+              // windows下  control + shift + s 保存
+              this.toggleFilterInput()
+            }
+          }
         }
-        return out
       }
-      // transformedValue () {
-      //   let result = ''
-      //   if (this.originValue !== '') {
-      //     let originValue = this.originValue
-      //     try {
-      //       this.currentJson = jsonlint.parse(originValue)
-      //       this.currentJsonStr = JSON.stringify(this.currentJson)
-      //       result = new JSONFormat(originValue, 4).toString()
-      //     } catch (e) {
-      //       result = '<span style="color: #f1592a; font-weight: bold;">' + e + '</span>'
-      //       this.currentJsonStr = result
-      //     }
-      //     return result
-      //   } else {
-      //     return ''
-      //   }
-      // }
+    })
+  },
+  methods: {
+    toggleFilterInput () {
+      var txt = window.getSelection ? window.getSelection() : document.selection.createRange().text
+      let input = document.querySelector('.find-input')
+      if (input) {
+        input.value = txt
+
+        input.dispatchEvent(this.inputEvent)
+      }
+      this.findInPage.openFindWindow()
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .tool_container {
+.tool_container {
+  height: 100%;
+  .input_box {
+    position: relative;
+    width: 100%;
     height: 100%;
-    .input_box {
+    background-color: #fff;
+    padding: 15px;
+    box-sizing: border-box;
+    .transformed_box {
       position: relative;
       width: 100%;
       height: 100%;
-      background-color: #fff;
-      padding: 15px;
-      box-sizing: border-box;
-      .transformed_box {
-        position: relative;
-        width: 100%;
-        height: 100%;
-        -webkit-user-select: text;
-        overflow-y: auto;
-        background-color: #282926;
-      }
+      -webkit-user-select: text;
+      overflow-y: auto;
+      background-color: #282926;
     }
   }
-  .trigger_area_trigger {
+}
+.trigger_area_trigger {
+  width: 20px;
+  height: 100%;
+  // margin-left: -10px;
+  cursor: ew-resize;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .trigger_area_trigger_item {
     width: 20px;
-    height: 100%;
-    // margin-left: -10px;
-    cursor: ew-resize;
+    height: 20px;
+    background-color: #333;
+    border-radius: 3px;
+    transition: all 0.2s ease-in-out;
+    margin: 8px 0;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
     justify-content: center;
-    .trigger_area_trigger_item {
-      width: 20px;
-      height: 20px;
-      background-color: #333;
-      border-radius: 3px;
-      transition: all 0.2s ease-in-out;
-      margin: 8px 0;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      &:hover {
-        transform: scale(1.1);
-      }
-      svg {
-        width: 16px;
-        height: 16px;
-        fill: #ffffff;
-      }
+    &:hover {
+      transform: scale(1.1);
+    }
+    svg {
+      width: 16px;
+      height: 16px;
+      fill: #ffffff;
     }
   }
+}
 </style>
--- src/renderer/components/tools/developer/Request/RequestResult.vue
+++ src/renderer/components/tools/developer/Request/RequestResult.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="request_result request_send_form">
+  <div class="request_result request_send_form"
+       id="jsonResult">
     <Tabs :animated="true">
       <TabPane label="Preview">
         <ResponsePreview :data="response.data"></ResponsePreview>
@@ -12,15 +13,6 @@
       </TabPane>
     </Tabs>
     <transition name="fade">
-      <div class="json_result_search">
-        <Input autofocus
-               size="small"
-               class="custom_input">
-        <span slot="prepend">搜索</span>
-        </Input>
-      </div>
-    </transition>
-    <transition name="fade">
       <div class="loading_container"
            v-if="requesting">
         <Loading></Loading>
@@ -30,68 +22,131 @@
 </template>
 
 <script>
-  import { Tabs, TabPane, Input } from 'view-design'
-  import { ipcRenderer } from 'electron'
-  export default {
-    name: 'RequestResult',
-    components: {
-      Tabs, TabPane, Input,
-      ResponsePreview: () => import('./ResponsePreview'),
-      Loading: () => import('../../../custom/Loading')
-    },
-    data () {
-      return {
-        response: {},
-        requesting: false
+import { Tabs, TabPane, Input } from 'view-design'
+import { ipcRenderer, remote } from 'electron'
+import { FindInPage } from 'electron-find'
+export default {
+  name: 'RequestResult',
+  components: {
+    Tabs, TabPane, Input,
+    ResponsePreview: () => import('./ResponsePreview'),
+    Loading: () => import('../../../custom/Loading')
+  },
+  data () {
+    return {
+      response: {},
+      requesting: false,
+      findInPage: null,
+      inputEvent: null
+    }
+  },
+  created () {
+    ipcRenderer.on('start-request', this.startRequest)
+    ipcRenderer.on('response', this.responseHandler)
+  },
+  mounted () {
+    this.findInPage = new FindInPage(remote.getCurrentWebContents(), {
+      parentElement: document.querySelector('#jsonResult'),
+      boxBgColor: '#333',
+      boxShadowColor: '#000',
+      inputColor: '#aaa',
+      inputBgColor: '#222',
+      inputFocusColor: '#555',
+      textColor: '#aaa',
+      textHoverBgColor: '#555',
+      caseSelectedColor: '#555',
+      offsetTop: 95,
+      preload: true
+    })
+
+    this.inputEvent = document.createEvent('HTMLEvents')
+    this.inputEvent.initEvent('input', true, true)
+
+    this.$nextTick(() => {
+      let isMac = (navigator.platform === 'Mac68K') || (navigator.platform === 'MacPPC') || (navigator.platform === 'Macintosh') || (navigator.platform === 'MacIntel')
+      isMac && (this.platform = 'mac')
+      window.onkeydown = (ev) => {
+        if (this.$route.name === 'request') {
+          if (this.platform === 'mac') {
+            if (ev.metaKey && ev.keyCode === 70) {
+              // mac下 command + shift + s
+              this.toggleFilterInput()
+            }
+          } else {
+            if (ev.ctrlKey && ev.keyCode === 70) {
+              // windows下  control + shift + s 保存
+              this.toggleFilterInput()
+            }
+          }
+        }
       }
+    })
+  },
+  methods: {
+    startRequest () {
+      this.requesting = true
     },
-    created () {
-      ipcRenderer.on('start-request', this.startRequest)
-      ipcRenderer.on('response', this.responseHandler)
+    responseHandler (event, response) {
+      this.response = response
+      setTimeout(() => {
+        this.requesting = false
+      }, 800)
     },
-    methods: {
-      startRequest () {
-        this.requesting = true
-      },
-      responseHandler (event, response) {
-        this.response = response
-        setTimeout(() => {
-          this.requesting = false
-        }, 800)
+    toggleFilterInput () {
+      var txt = window.getSelection ? window.getSelection() : document.selection.createRange().text
+      let input = document.querySelector('.find-input')
+      if (input) {
+        input.value = txt
+
+        input.dispatchEvent(this.inputEvent)
       }
+      this.findInPage.openFindWindow()
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .request_result {
-    position: relative;
-    height: 100%;
-    .json_result_search {
+.request_result {
+  position: relative;
+  height: 100%;
+  .json_result_search {
+    position: absolute;
+    left: 0;
+    top: 46px;
+    width: 100%;
+    height: 36px;
+    background-color: rgba(0, 0, 0, 0.7);
+    // background-color: rgba(94, 160, 33, 0.7);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    .filter_result {
       position: absolute;
-      left: 0;
-      top: 46px;
-      width: 100%;
-      height: 36px;
-      background-color: rgba(0, 0, 0, 0.7);
-      // background-color: rgba(94, 160, 33, 0.7);
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-    }
-    .loading_container {
-      position: absolute;
-      width: 100%;
-      height: 100%;
+      right: 15px;
       top: 0;
-      left: 0;
-      z-index: 1;
-      background-color: rgba(0, 0, 0, 0.8);
+      height: 36px;
+      padding: 0 15px;
+      color: #c8c8c8;
       display: flex;
       flex-direction: row;
       align-items: center;
       justify-content: center;
     }
   }
+  .loading_container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+}
 </style>
