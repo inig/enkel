ca8c7e5e608128e1957e40bfda0933891844505f
liangshan <loveliangshan@126.com>
2020-03-04 14:34:26 +0800

    no message

--- src/renderer/components/tools/media/Flac/Search.vue
+++ src/renderer/components/tools/media/Flac/Search.vue
@@ -52,218 +52,219 @@
 </template>
 
 <script>
-import { ipcRenderer } from 'electron'
-import { Icon, Input, Spin } from 'view-design'
-import lottie from 'lottie-web'
-export default {
-  name: 'MediaFlacSearch',
-  components: {
-    Icon, Input, Spin
-  },
-  props: {
-    shown: {
-      type: Boolean
-    }
-  },
-  data () {
-    return {
-      kw: '',
-      isSearching: false,
-      list: [],
-      activeIndex: -1,
-      isLoading: false,
-      lottie: {
-        empty: null
+  import { ipcRenderer } from 'electron'
+  import { Icon, Input, Spin } from 'view-design'
+  import lottie from 'lottie-web'
+  export default {
+    name: 'MediaFlacSearch',
+    components: {
+      Icon, Input, Spin
+    },
+    props: {
+      shown: {
+        type: Boolean
       }
-    }
-  },
-  mounted () {
-    ipcRenderer.on('flac-response-search', this.searchResponse)
-    ipcRenderer.on('flac-response-real-path', this.requesting)
-    this.initLottieEmpty()
-  },
-  methods: {
-    doSearch () {
-      if (!this.kw || !this.kw.trim()) {
-        return
+    },
+    data () {
+      return {
+        kw: '',
+        isSearching: false,
+        list: [],
+        activeIndex: -1,
+        isLoading: false,
+        lottie: {
+          empty: null
+        }
       }
-      this.isSearching = true
-      ipcRenderer.send('flac-search', {
-        kw: this.kw
-      })
     },
-    searchResponse (event, data) {
-      this.list = data
-      this.isSearching = false
+    mounted () {
+      ipcRenderer.on('flac-response-search', this.searchResponse)
+      ipcRenderer.on('flac-response-real-path', this.requesting)
+      this.initLottieEmpty()
     },
-    chooseItem (index, url) {
-      this.activeIndex = index
-      this.isLoading = true
-      ipcRenderer.send('flac-get-real-path', {
-        url: url
-      })
-    },
-    requesting () {
-      this.isLoading = false
-    },
-    initLottieEmpty () {
-      this.$nextTick(() => {
-        this.lottie.empty = lottie.loadAnimation({
-          container: this.$refs.emptyLottieRef,
-          renderer: 'svg',
-          loop: true,
-          autoplay: true,
-          animationData: require('../../../../assets/lottie/empty.json')
-        })
-      })
-    }
-  },
-  watch: {
-    shown (val) {
-      if (!val) {
+    methods: {
+      doSearch () {
+        if (!this.kw || !this.kw.trim()) {
+          return
+        }
+        this.isSearching = true
         this.activeIndex = -1
+        ipcRenderer.send('flac-search', {
+          kw: this.kw
+        })
+      },
+      searchResponse (event, data) {
+        this.list = data
+        this.isSearching = false
+      },
+      chooseItem (index, url) {
+        this.activeIndex = index
+        this.isLoading = true
+        ipcRenderer.send('flac-get-real-path', {
+          url: url
+        })
+      },
+      requesting () {
+        this.isLoading = false
+      },
+      initLottieEmpty () {
+        this.$nextTick(() => {
+          this.lottie.empty = lottie.loadAnimation({
+            container: this.$refs.emptyLottieRef,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../../../../assets/lottie/empty.json')
+          })
+        })
+      }
+    },
+    watch: {
+      shown (val) {
+        if (!val) {
+          this.activeIndex = -1
+        }
       }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
-.flac_search {
-  position: relative;
-  // padding-top: 5px;
-  width: 100%;
-  height: 422px;
-  // height: 100%;
-  background-color: #f8f8f8;
-  overflow: auto;
-  &_header {
-    position: sticky;
-    top: 0;
-    z-index: 2;
-    background-color: #17191e;
+  .flac_search {
+    position: relative;
+    // padding-top: 5px;
     width: 100%;
-    height: 48px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    color: #c8c8c8;
-    font-size: 13px;
-    padding-left: 10px;
-    border-bottom: 1px solid #222;
-    &_btn {
-      width: 80px;
+    height: 422px;
+    // height: 100%;
+    background-color: #f8f8f8;
+    overflow: auto;
+    &_header {
+      position: sticky;
+      top: 0;
+      z-index: 2;
+      background-color: #17191e;
+      width: 100%;
       height: 48px;
       display: flex;
       flex-direction: row;
       align-items: center;
-      justify-content: center;
-      font-size: 14px;
       color: #c8c8c8;
-      cursor: pointer;
-      background-color: #222;
-      &:hover {
-        opacity: 0.7;
-      }
-    }
-  }
-  &_content {
-    position: relative;
-    width: 100%;
-    height: calc(~"100% - 48px");
-    background-color: lightblue;
-    overflow-x: hidden;
-    overflow-y: auto;
-    &_main {
-      width: 100%;
-      min-height: 100%;
-      background-color: #f5f5f5;
-      &_header {
-        width: 100%;
+      font-size: 13px;
+      padding-left: 10px;
+      border-bottom: 1px solid #222;
+      &_btn {
+        width: 80px;
         height: 48px;
-        padding: 0 10px;
-        box-sizing: border-box;
-        font-size: 13px;
-        color: #666;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-start;
-        border-bottom: 1px solid #eee;
-      }
-      &_item {
-        width: 100%;
-        // height: 48px;
-        padding: 8px 15px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: flex-end;
-        cursor: pointer;
-        &.active {
-          background-color: #4fc08d !important;
-          color: #fff !important;
-        }
-        &:nth-child(odd) {
-          background-color: #eee;
-        }
-        &:hover {
-          background-color: #e8e8e8;
-        }
-        &_text {
-          line-height: 20px;
-          font-size: 12px;
-          flex: 1;
-        }
-        &_loading {
-          margin-left: -8px;
-          margin-right: 8px;
-        }
-      }
-    }
-    &_blank {
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      height: 100%;
-      background-color: #fff;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      .empty_result {
-        width: 280px;
-        height: 280px;
-        margin-bottom: 50px;
-      }
-      img {
-        width: 100%;
-      }
-      &_text {
-        width: 100%;
-        height: 32px;
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: center;
         font-size: 14px;
-        color: rgb(95, 105, 113);
-        margin-top: -50px;
+        color: #c8c8c8;
+        cursor: pointer;
+        background-color: #222;
+        &:hover {
+          opacity: 0.7;
+        }
       }
     }
+    &_content {
+      position: relative;
+      width: 100%;
+      height: calc(~"100% - 48px");
+      background-color: lightblue;
+      overflow-x: hidden;
+      overflow-y: auto;
+      &_main {
+        width: 100%;
+        min-height: 100%;
+        background-color: #f5f5f5;
+        &_header {
+          width: 100%;
+          height: 48px;
+          padding: 0 10px;
+          box-sizing: border-box;
+          font-size: 13px;
+          color: #666;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: flex-start;
+          border-bottom: 1px solid #eee;
+        }
+        &_item {
+          width: 100%;
+          // height: 48px;
+          padding: 8px 15px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: flex-end;
+          cursor: pointer;
+          &.active {
+            background-color: #4fc08d !important;
+            color: #fff !important;
+          }
+          &:nth-child(odd) {
+            background-color: #eee;
+          }
+          &:hover {
+            background-color: #e8e8e8;
+          }
+          &_text {
+            line-height: 20px;
+            font-size: 12px;
+            flex: 1;
+          }
+          &_loading {
+            margin-left: -8px;
+            margin-right: 8px;
+          }
+        }
+      }
+      &_blank {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .empty_result {
+          width: 280px;
+          height: 280px;
+          margin-bottom: 50px;
+        }
+        img {
+          width: 100%;
+        }
+        &_text {
+          width: 100%;
+          height: 32px;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+          font-size: 14px;
+          color: rgb(95, 105, 113);
+          margin-top: -50px;
+        }
+      }
+    }
+    &_loading {
+      position: fixed;
+      height: calc(~"100% - 250px");
+      width: 100%;
+      left: 0;
+      bottom: 0;
+      background-color: #fff;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+    }
   }
-  &_loading {
-    position: fixed;
-    height: calc(~"100% - 250px");
-    width: 100%;
-    left: 0;
-    bottom: 0;
-    background-color: #fff;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-}
 </style>
