e3e4c4953e84aa24084684f60a3a6d2f1b396966
lsliangshan <loveliangshan@126.com>
2020-04-16 01:12:23 +0800

    no message

--- src/renderer/components/pages/Profile/ChatWindow.vue
+++ src/renderer/components/pages/Profile/ChatWindow.vue
@@ -1,38 +1,489 @@
 <template>
-  <div class="container"
-       @click="hide">
-    <code>
-      <pre>{{JSON.stringify(info, null, 2)}}</pre>
-    </code>
+  <div class="chat_window">
+    <div class="chat_window_header">
+      <div class="chat_window_header_back"
+           @click="hide">
+        <Icon type="ios-arrow-back"
+              style="pointer-events: none;"
+              size="20" />
+      </div>
+      {{friendInfo.memo_name || friendInfo.nickname || friendInfo.username}}
+    </div>
+    <div class="chat_window_content">
+      <MsgBox v-for="(item, index) in info"
+              :key="item.msg_id"
+              style="margin-top: 20px;"
+              :user-info="userInfo"
+              :friend-info="friendInfo"
+              :info="item"></MsgBox>
+      <code>
+        <pre>{{JSON.stringify(info[0], null, 2)}}</pre>
+      </code>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'ProfileChatWindow',
-    props: {
-      info: {
-        type: [Object, Array],
-        default () {
-          return {}
-        }
+import { Icon } from 'view-design'
+import MsgBox from './MsgBox'
+export default {
+  name: 'ProfileChatWindow',
+  components: {
+    Icon,
+    MsgBox
+  },
+  props: {
+    info: {
+      type: [Object, Array],
+      default () {
+        return []
       }
     },
-    methods: {
-      hide () {
-        this.$emit('hide')
+    friendInfo: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    userInfo: {
+      type: Object,
+      default () {
+        return {}
       }
     }
+  },
+  data () {
+    return {
+      msgs: [
+        {
+          "content": {
+            "create_time": 1586880914,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "test"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586880653,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586880914403,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6943837506,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586942135,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "1111"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586942135908,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952349413,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586943404,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "Nice to meet U"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586943404982,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952559127,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586943565,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "111111"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586943565193,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952587131,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586943740,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "123"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586943740940,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952560992,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586944948,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "Hello"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586944948790,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952825059,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586945053,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "Hello 2"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586945053637,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952842923,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586945076,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "测试"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586945076395,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6952847127,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586951901,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000001",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "测试消息"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586939595,
+            "target_id": "18000000002",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586951901497,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6953999740,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586961684,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "Nice to Meet U"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586961684834,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6956026876,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586962100,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "asssssss"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586962100421,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6956117748,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586962583,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "kkkkkkk"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586962583880,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6956277639,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        },
+        {
+          "content": {
+            "create_time": 1586962717,
+            "from_appkey": "41a433f900631257d7a6ebb9",
+            "from_id": "18000000002",
+            "from_platform": "api",
+            "from_type": "user",
+            "msg_body": {
+              "extras": {},
+              "text": "111111111111"
+            },
+            "msg_type": "text",
+            "sui_mtime": 1586933421,
+            "target_id": "18000000001",
+            "target_type": "single",
+            "version": 1
+          },
+          "ctime_ms": 1586962717703,
+          "custom_notification": {
+            "alert": "",
+            "at_prefix": "",
+            "enabled": false,
+            "title": ""
+          },
+          "msg_id": 6956249114,
+          "msg_level": 0,
+          "msg_type": 3,
+          "need_receipt": false
+        }
+      ]
+    }
+  },
+  methods: {
+    hide () {
+      this.$emit('hide')
+    }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .container {
-    position: absolute;
-    left: 0;
-    top: 0;
+.chat_window {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  &_header {
+    position: relative;
     width: 100%;
-    height: 100%;
-    background-color: #fff;
+    height: 40px;
+    border-bottom: 1px solid #f5f5f5;
+    box-sizing: border-box;
+    box-shadow: 0 1px 20px 1px #f5f5f5;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    &_back {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 40px;
+      height: 40px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      opacity: 1;
+      transition: all 0.2s ease-in-out;
+      &:hover {
+        opacity: 0.7;
+      }
+    }
   }
+  &_content {
+    width: 100%;
+    height: calc(~"100% - 40px");
+    overflow-y: auto;
+  }
+}
 </style>
---
+++ src/renderer/components/pages/Profile/MsgBox.vue
@@ -0,0 +1,233 @@
+<template>
+  <div class="msg_box">
+    <!-- {{JSON.stringify(info)}} -->
+    <div class="msg_box_left"
+         v-if="userInfo.username !== info.content.from_id">
+      <div class="msg_box_left_avatar">
+        <Avatar size="34"
+                shape="square"
+                style="background-color: #fff;"
+                :src="friendInfo.headIcon"
+                v-if="friendInfo.headIcon"></Avatar>
+        <Avatar size="34"
+                shape="square"
+                icon="ios-person"
+                style="background-color: #87d068"
+                v-else></Avatar>
+      </div>
+      <div class="msg_box_left_content">
+        <div class="msg_box_left_content_box"
+             :style="[leftTheme]"
+             v-html="info.content.msg_body.text.split('\\n').map(item => `<p>${item}</p>`).join('')"></div>
+        <div class="msg_box_left_content_arrow">
+          <div class="triangle"
+               :style="[leftTheme]"></div>
+        </div>
+      </div>
+    </div>
+    <div class="msg_box_right"
+         v-else>
+      <div class="msg_box_right_content">
+        <div class="msg_box_right_content_box"
+             :style="[rightTheme]"
+             v-html="info.content.msg_body.text.split('\\n').map(item => `<p>${item}</p>`).join('')"></div>
+        <div class="msg_box_right_content_arrow">
+          <div class="triangle"
+               :style="[rightTheme]"></div>
+        </div>
+      </div>
+      <div class="msg_box_right_avatar">
+        <Avatar size="34"
+                shape="square"
+                style="background-color: #fff;"
+                :src="userInfo.headIcon"
+                v-if="userInfo.headIcon"></Avatar>
+        <Avatar size="34"
+                shape="square"
+                icon="ios-person"
+                style="background-color: #87d068"
+                v-else></Avatar>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Avatar } from 'view-design'
+export default {
+  name: 'ChatWindowMsgBox',
+  components: {
+    Avatar
+  },
+  props: {
+    info: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    friendInfo: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    userInfo: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      bgColor: '#eee',
+      leftTheme: {
+        backgroundColor: '#eee',
+        color: '#555'
+      },
+      rightTheme: {
+        backgroundColor: '#19be6b',
+        color: '#fff'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.msg_box {
+  width: 100%;
+  min-height: 34px;
+  overflow: hidden;
+  &_left {
+    width: 100%;
+    min-height: 34px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-start;
+    &_avatar {
+      width: 34px;
+      height: 34px;
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: center;
+    }
+    &_content {
+      position: relative;
+      width: 200px;
+      min-height: 34px;
+      // background-color: lightseagreen;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      &_box {
+        max-width: 190px;
+        min-height: 34px;
+        border-radius: 4px;
+        line-height: 1.5;
+        padding: 8px 10px;
+        box-sizing: border-box;
+        font-size: 13px;
+        color: #555;
+        margin-left: 10px;
+        -webkit-user-select: text;
+        -webkit-app-region: none;
+      }
+      &_arrow {
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        left: 5px;
+        top: 12px;
+        transform: rotate(30deg);
+        // background-color: red;
+      }
+    }
+  }
+  &_right {
+    width: 100%;
+    min-height: 34px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: flex-end;
+    &_avatar {
+      width: 34px;
+      height: 34px;
+      display: flex;
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: center;
+    }
+    &_content {
+      position: relative;
+      width: 200px;
+      min-height: 34px;
+      // background-color: lightseagreen;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-end;
+      &_box {
+        max-width: 190px;
+        min-height: 34px;
+        border-radius: 4px;
+        line-height: 1.5;
+        padding: 8px 10px;
+        box-sizing: border-box;
+        font-size: 13px;
+        font-weight: 400;
+        margin-right: 10px;
+        -webkit-user-select: text;
+        -webkit-app-region: none;
+      }
+      &_arrow {
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        right: 0;
+        top: 12px;
+        transform: rotate(-30deg);
+        // background-color: red;
+      }
+    }
+  }
+}
+.triangle {
+  position: relative;
+  // background-color: orange;
+  text-align: left;
+}
+.triangle:before,
+.triangle:after {
+  content: "";
+  position: absolute;
+  background-color: inherit;
+}
+.triangle,
+.triangle:before,
+.triangle:after {
+  width: 5px;
+  height: 5px;
+  border-top-right-radius: 30%;
+}
+
+.triangle {
+  transform: rotate(-60deg) skewX(-30deg) scale(1, 0.866);
+}
+.triangle:before {
+  transform: rotate(-135deg) skewX(-45deg) scale(1.414, 0.707)
+    translate(0, -50%);
+}
+.triangle:after {
+  transform: rotate(135deg) skewY(-45deg) scale(0.707, 1.414) translate(50%);
+}
+</style>
